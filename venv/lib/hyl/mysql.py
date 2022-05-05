2dd108bbc0c47bf6e21b8cda38c0b4c76924443a
huyunlong007 <huyunlongxi@icloud.com>
2022-05-05 17:40:00 +0800

    hyl

---
+++ venv/lib/hyl/mysql.py
@@ -0,0 +1,26 @@
+import pymysql
+
+# 第一步：建立连接
+conn = pymysql.connect(host='localhost', port=3306, user='root', password='HYL941128', database='mysql',
+                       charset='utf8mb4')
+# print(conn)
+try:
+    # 第二步：获得游标对象
+    # cursor = conn.cursor()       #返回一个游标对象   这个对象可以执行sql语句
+    with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:  # 将默认的输出元组改为输出为字典
+        # 第三步：通过游标向数据库服务器发出sql语句，获取执行结果
+        sql = '''
+
+
+        '''
+        cursor.execute('sql')
+        # 第四步：提交上面的操作
+        conn.commit()
+except pymysql.MySQLError as err:
+    # 第四步：回滚（提交失败）  不需要增删改时，不使用回滚
+    conn.rollback()
+finally:
+    # 关闭连接（释放资源）
+    conn.close()
+
+
