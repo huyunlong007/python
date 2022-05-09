22f5a2df92c4406ab5a358a724e78e1ef8d85fed
huyunlong007 <huyunlongxi@icloud.com>
2022-05-09 20:23:51 +0800

    hyl

--- venv/lib/hyl/study.py
+++ venv/lib/hyl/study.py
@@ -1,3 +1,11 @@
 
 message="hello,world!"
 print(message)
+
+simple_message="i believe i can fly!"
+print(simple_message)
+
+for i in range(1,10):
+    for j in range(1,i+1):
+       print('%s*%s=%s'%(i,j,i*j),end='')
+    print()
