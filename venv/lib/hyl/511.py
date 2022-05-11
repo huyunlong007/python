92fab00d2c3275efab3f9505015d37ef93285fab
huyunlong007 <huyunlongxi@icloud.com>
2022-05-11 15:42:22 +0800

    hyl

--- venv/lib/hyl/511.py
+++ venv/lib/hyl/511.py
@@ -17,6 +17,11 @@ motorcycle.append('bmw') #在末尾添加元素
 print(motorcycle)
 motorcycle.insert(1,'linmu') #添加元素
 print(motorcycle)
-del motorcycle[1] #删除元素
+del motorcycle[1] #del删除元素,后面不再使用该元素
 print(motorcycle)
+
+poped_motorcycle=motorcycle.pop() #pop删除末尾元素，并能接着使用
+print(motorcycle)
+print(poped_motorcycle)
+
 
