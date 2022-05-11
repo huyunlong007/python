8a1d011f6f707a241b21ccde84b533e972432ea8
huyunlong007 <huyunlongxi@icloud.com>
2022-05-11 19:44:17 +0800

    game

--- Users/huyunlong/miniforge3/lib/python3.9/airplane.py
+++ Users/huyunlong/miniforge3/lib/python3.9/airplane.py
@@ -1,120 +1,81 @@
-#飞机子弹的图片路径
-        self.bullet=bullet;
-        #飞机的类型，0代表自己，1代表敌人
-        self.type=type;
-        #飞机的宽度
-        self.width=w;
-        #飞机的高度
-        self.height=h;
-        #存放子弹的数组
-        self.allbullet=[];
-        #用于判断子弹多长时间发射一次和fps相关
-        self.time=0;
-        #当前这个飞机的速度
-        self.speedx=1;
-        self.speedy=1;
-        # 攻击速度,值越小攻击越快最小为1
-        self.advancespeed = advancespeed;
-        #每个飞机自己携带的分数，目前随机
-        self.myselfMark=random.randint(1,3)
-        #运动轨迹
-        self.runType=random.randint(0, 3)
-        #是否死亡 1表示死亡 0表示活着
-        self.isDeath=0
-        #爆炸图片播放那一张
-        self.showboomTime=0
-        #爆炸数据
-        self.boom=boom
-        #开始攻击
-        self.attack();
-    def death(self):
-        #死亡状态
-        self.isDeath=1;
-    def showboom(self):
-        #显示boom的动画
-        if self.showboomTime==len(self.boom)-1:
-            return ;
-        if self.time%5==0:
-            self.showboomTime+=1;
-        self.screen.blit(self.boom[self.showboomTime], (self.x, self.y))
-    def run(self,x,y):
-        if self.isDeath==1:
-            self.showboom();
-            return 1;
-        self.x = x - self.width / 2;
-        self.y = y - self.height / 2;
-        self.screen.blit(self.img, (self.x, self.y))
-    def attack(self):
-        #飞机发射子弹的函数
-        if self.time%self.advancespeed==0 and self.isDeath==0:
-            self.allbullet.append(bullet(self.screen,self.bullet,self.x-self.width/2,self.y,self.width,self.height,128,128,self.type))
-        self.time+=1
-    def ai(self):
-        if self.isDeath==1:
-            self.showboom();
-            return
-        #这里分为四种的运动轨迹随机决定
-        if self.type==1:
-            if self.runType==0:
-                self.y+=self.speedy*2;
-            elif self.runType==1:
-                self.y+=self.speedy;
-                if self.x<=0-self.width/2 or self.x>=500-self.width/2:
-                    self.speedx=0-self.speedx
-                self.x+=self.speedx
-            elif self.runType==2:
-                self.y += self.speedy;
-                if self.x<=0-self.width/2 or self.x>=500-self.width/2:
-                    self.speedx = 0 - self.speedx
-                self.x -= self.speedx
-            elif self.runType==3:
-                self.y += self.speedy;
-                if self.x<=0-self.width/2 or self.x>=500-self.width/2:
-                    self.speedx = 0 - self.speedx
-                    self.speedy=0-self.speedy
-                self.x -= self.speedx
-            self.screen.blit(self.img, (self.x, self.y))
-            self.attack()
-    def __del__(self):
-        return
+import pygame
 
-class bullet:
-    def __init__(self,screen,img,x,y,airplanex,airplaney,w,h,type):
-        #游戏场景传参
-        self.screen=screen;
-        #子弹的图片的路径
-        self.img=img;
-        if type==0:
-            # 子弹的y坐标
-            self.y=y-airplaney;
-            # 子弹的x坐标
-            self.x = x + airplanex / 2;
-        else:
-            self.y=y+airplaney-20;
-            self.x = x+airplanex/2+w/2-12;
-        #子弹图片的宽度
-        self.width=w
-        #子弹图片的高度
-        self.height=h
-        #子弹需要一直往前飞
-        self.run()
-        #子子弹是谁的
-        self.type = type
-        if self.type==0:
-            self.speed=5
-        else:
-            self.speed=3
-    def advance(self):
-        if self.type==0:
-            self.y-=self.speed;
-        else:
-            self.y+=self.speed;
-        self.run();
+# 玩家飞机类,pygame.sprite模块里面包含了一个名为Sprite类，他是pygame本身自带的一个精灵。
+class MyPlane(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        # convert_alpha()更改图像的像素格式，包括每个像素的alpha,相当于图片背景变为透明
+        self.image1 = pygame.image.load('images/me1.png').convert_alpha()
+        self.image2 = pygame.image.load('images/me2.png').convert_alpha()
+        # 飞机摧毁图片,以数字形式保存
+        self.destory_image = []
+        self.destory_image.extend([
+            pygame.image.load('images/me_destroy_1.png').convert_alpha(),
+            pygame.image.load('images/me_destroy_2.png').convert_alpha(),
+            pygame.image.load('images/me_destroy_3.png').convert_alpha(),
+            pygame.image.load('images/me_destroy_4.png').convert_alpha()
+        ])
+        # 定义屏幕宽高
+        self.width = bg_size[0]
+        self.height = bg_size[1]
 
-    def run(self):
-        self.screen.blit(self.img, (self.x, self.y))
-    def __del__(self):
-        return
+        # get_rect()是一个处理矩形图像的方法，返回值包含矩形的居中属性,这里返回飞机图片1的位置,可以获取图片的宽高等属性
+        self.rect = self.image1.get_rect()
 
+        # 飞机的初始化位置,//是整除，位置居中以及高度为图片下框离屏幕最下方60
+        self.rect.left = (self.width - self.rect.width)//2
+        self.rect.top = self.height - self.rect.height - 60
+
+        # 设置飞机的速度
+        self.myPlaneSpeed = 10
+        self.active = True
+
+        # 设置飞机是否是无敌状态（重生3秒内无敌）
+        self.invincible = True
+
+        # 飞机碰撞检测，会忽略掉图片中白色的背景部分,从指定 Surface 对象中返回一个 Mask
+        # 用于快速实现完美的碰撞检测，Mask 可以精确到 1 个像素级别的判断。
+        # Surface 对象中透明的部分设置为 1，不透明部分设置为 0。
+        self.mask = pygame.mask.from_surface(self.image1)
+
+    # 玩家飞机向上移动
+    def moveUp(self):
+        # 说明还没定格，即还未到达游戏界面上边界
+        if self.rect.top > 0:
+            self.rect.top -= self.myPlaneSpeed
+        # 说明移动到达上边界了
+        else:
+            self.rect.top = 0
+
+    # 玩家飞机向下移动
+    def moveDown(self):
+        # 底部需要划出60的高度用来展示其他数据（炸弹数，生命数等）
+        if self.rect.bottom < self.height - 60:
+            # self.rect.bottom指的是飞机图片下边界
+            self.rect.bottom += self.myPlaneSpeed
+        else:
+            self.rect.bottom = self.height - 60
+
+    # 玩家飞机向左移动
+    def moveLeft(self):
+        if self.rect.left > 0:
+            self.rect.left -= self.myPlaneSpeed
+        else:
+            self.rect.left = 0
+
+    # 玩家飞机向右移动
+    def moveRight(self):
+        if self.rect.right < self.width:
+            self.rect.right += self.myPlaneSpeed
+        else:
+            self.rect.right = self.width
+
+    # 玩家飞机重生
+    def reset(self):
+        self.active = True
+        # 重生时处于无敌状态
+        self.invincible = True
+        # 重生飞机的初始化位置,//是整除，位置居中以及高度为图片下框离屏幕最下方60
+        self.rect.left = (self.width - self.rect.width) // 2
+        self.rect.top = self.height - self.rect.height - 60
 
 
--- Users/huyunlong/miniforge3/lib/python3.9/main.py
+++ Users/huyunlong/miniforge3/lib/python3.9/main.py
@@ -1,203 +1,105 @@
-# -*- coding: UTF-8 -*-
-# animation.py
-
-# 导入需要的模块
 import pygame
-from pygame.locals import *
 import sys
-import airplane
-import random
-class Game:
-  def __init__(self):
-    # 初始化pygame
-    pygame.init()
-    #初始化字体文件
-    pygame.font.init()
-    #初始化游戏分数
-    self.mark=0
-    # 设置帧率（屏幕每秒刷新的次数）
-    self.FPS = 60
+import traceback
+from pygame.locals import *
+from random import *
+import myplane
 
-    # 获得pygame的时钟
-    self.fpsClock = pygame.time.Clock()
+# 初始化
+pygame.init()
+# 设置窗口大小
+bg_size = width, height = 400, 700  # 实际上是元组
+screen = pygame.display.set_mode(bg_size)  # 设置窗口
+pygame.display.set_caption("飞机大战")  # 窗口标题
+# 加载背景图片,对于普通图像的显示效果有没有convert都是一样的，但是 使用 convert 可以转换格式，提高 blit 的速度
+background = pygame.image.load("images/background.png").convert()
 
-    # 设置窗口大小
-    self.screen = pygame.display.set_mode((500, 800), 0, 32)
+def main():
+    # 创建时钟对象（可以控制游戏循环频率）
+    clock = pygame.time.Clock()
 
-    # 设置标题
-    pygame.display.set_caption('飞机大战')
+    # 生成玩家飞机
+    me = myplane.MyPlane(bg_size)
 
-    # 定义颜色
-    self.WHITE = (255, 255, 255)
-    #用于存放爆炸图片
-    self.boom=[pygame.image.load("boom_1.gif")
-      ,pygame.image.load("boom_2.gif")
-      ,pygame.image.load("boom_3.gif")
-      ,pygame.image.load("boom_4.gif")
-      ,pygame.image.load("boom_5.gif")
-      ,pygame.image.load("boom_6.gif")
-      , pygame.image.load("boom_7.gif")
-      , pygame.image.load("boom_8.gif")
-      , pygame.image.load("boom_9.gif")]
-    # 加载一张背景图片
-    self.img = pygame.image.load('bg.jpg')
-    #加载开始游戏按钮
-    self.startGameImage=pygame.image.load('start.png')
+    # 玩家三条命
+    life_num = 3
 
-    # 初始化背景图片的位置
-    self.imgx = 0
-    self.imgy = 0
-    # 全局的时间变量
-    self.time = 0;
-    #敌人子弹的图片数据
-    self.allEnemyButtlueImg=[pygame.image.load('buttle3.png')]
-    # 存放所有敌人图片数据的一个列表
-    self.allEnemyImg = [pygame.image.load('em1.png'), pygame.image.load('em2.png')]
-    # 存放所有敌人变量的一个列表
-    self.allEnemy = [];
-    #是否开始了游戏 开始游戏状态为0 死亡状态为1 为开始为2
-    self.isStart=2
-    #最后一个爆炸动画
-    self.lastTime=0
-    #开始执行动画
-    self.run()
-  def initGame(self):
-    # 创建一个自己角色
-    self.myself = airplane.airplane(self.screen, 30, pygame.image.load('airplane.png'), 250, 800,
-                                    pygame.image.load('buttle.png'),
-                                    0, 120, 79, self.boom)
-    self.isStart=0
-  def run(self):
-    # 程序主循环
-    while True:
+    # 游戏暂停,默认为非暂停状态
+    paused = False
 
-      # 每次都要重新绘制背景白色
-      self.screen.fill(self.WHITE)
+    # 控制玩家飞机图片切换，展示突突突的效果
+    switch_image = True
+    # 切换延时
+    delay = 100
 
-      # 背景的循环滚动
-      self.imgy += 2
-      if self.imgy == 854:
-        self.imgy = 0
-      # 背景的绘制，绘制两张图片实现循环滚动的效果
-      self.screen.blit(self.img, (self.imgx, self.imgy))
-      self.screen.blit(self.img, (self.imgx, self.imgy - 854))
-      #如果游戏isstart为flase则游戏不会继续刷新
-      if self.isStart==0:
-        self.startGame();
-      elif self.isStart == 1:
-        self.clearAll();
-      else:
-        self.showstartGame()
-      for event in pygame.event.get():
-        if event.type == QUIT:
-          pygame.quit()
-          sys.exit()
-        if event.type==MOUSEBUTTONDOWN:
-          x, y = pygame.mouse.get_pos()
-          if(x>170 and x<170+161and y>300 and y<300+43):
-            self.isStart=0
-            self.initGame();
+    running = True
+    while running:
+        # 获取事件
+        for event in pygame.event.get():
+            # 结束事件触发结束操作
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
 
-      # 刷新屏幕
-      pygame.display.update()
-
-      # 设置pygame时钟的间隔时间
-      self.fpsClock.tick(self.FPS)
-  def showMark(self):
-    a = pygame.font.SysFont('幼圆', 50)
-    text = a.render(str(self.mark),True,(255,0,0))
-    b=len(str(self.mark))
-    self.screen.blit(text, [500-b*30,10])
-  def showstartGame(self):
-    self.screen.blit(self.startGameImage,(170,300))
-  def addMark(self,m):
-    self.mark+=m;
-  def clearMark(self):
-    self.mark=0;
-  def startGame(self):
-    # 获取鼠标位置点
-    x, y = pygame.mouse.get_pos()
-    # 飞机的运行函数
-    self.myself.run(x, y);
-    # 所有子弹每一帧的位置
-    for item in self.myself.allbullet:
-      if item.y <= 0:
-        # 当子弹行驶出地图边界之后移除列表中的当前元素并且销毁对象
-        self.myself.allbullet.remove(item)
-        del item
-      else:
-        # 如果没有行驶出边界范围则继续行驶
-        item.advance()
-        for enemy in self.allEnemy:
-          try:
-            # 判断我方子弹是否与敌人相撞，并且相撞的时候敌人还活着
-            if (
-                    item.x > enemy.x and item.x < enemy.x + enemy.width and item.y >= enemy.y and item.y < enemy.y + enemy.height and enemy.isDeath == 0):
-              self.myself.allbullet.remove(item)
-              del item
-              enemy.death()
-              self.addMark(enemy.myselfMark)
-          except:
-            continue;
-    self.showMark()
-    # 发射子弹的函数具体的射速由对象内部决定
-    self.myself.attack();
-    # 外部计算时间的一个变量没time % FPS == 0 时为1秒钟
-    self.time += 1
-    if self.time==self.lastTime:
-      self.isStart=1
-    # 飞机随机生成时间
-    if self.time % (self.FPS * 2) == 0:
-      # 每过一秒钟新生成一架飞机，添加到allEnemy这个列表之中，y从0开始 x在窗口大小随机生成
-      self.allEnemy.append(
-        airplane.airplane(self.screen, 15, self.allEnemyImg[random.randint(0, 1)], random.randint(0, 500 - 179), -134,
-                          self.allEnemyButtlueImg[0], 1, 179, 134, self.boom))
-    # 循环执行该列表，敌人飞机的运动轨迹
-    for item in self.allEnemy:
-      # 飞机超出下边界后去除
-      if item.y > 800 + item.height:
-        self.allEnemy.remove(item)
-        del item
-      # 飞机死亡并且存在子弹数为0后去除
-      elif item.isDeath == 1 and len(item.allbullet) == 0:
-        self.allEnemy.remove(item)
-        del item
-      # 否则飞机继续运行
-      else:
-        item.ai()
-        item.attack()
-        for i in item.allbullet:
-          if i.y <= -96 or i.y >= 896:
-            # 当子弹行驶出地图边界之后移除列表中的当前元素并且销毁对象
-            item.allbullet.remove(i)
-            del i
-          else:
-            # 如果没有行驶出边界范围则继续行驶
-            if (
-                    i.x > self.myself.x and i.x < self.myself.x + self.myself.width and i.y >= self.myself.y and i.y < self.myself.y + self.myself.height and self.myself.isDeath == 0):
-              self.myself.death()
-              self.lastTime=self.time+50;
-            i.advance()
-
-  def clearAll(self):
-    self.mark=0
-    for i in self.allEnemy:
-      for j in i.allbullet:
-        del j;
-      del i;
-    for j in self.myself.allbullet:
-      del j
-    del self.myself
-    # 存放所有敌人变量的一个列表
-    self.allEnemy = [];
-    # 是否开始了游戏
-    self.isStart = 2
-    self.lastTime=0
+        # 检测用户键盘操作,分别为上下左右
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[K_w] or key_pressed[K_UP]:
+            me.moveUp()
+        if key_pressed[K_s] or key_pressed[K_DOWN]:
+            me.moveDown()
+        if key_pressed[K_a] or key_pressed[K_LEFT]:
+            me.moveLeft()
+        if key_pressed[K_d] or key_pressed[K_RIGHT]:
+            me.moveRight()
 
 
-game=Game()
+        # 在屏幕上面绘制背景图像，并指定位置
+        screen.blit(background, (0, 0))
 
+        # 绘制子弹补给、炸弹补给等各种元素
+        # 未暂停且生命大于0
+        if paused == False and life_num > 0:
+            # 绘制玩家飞机,如果飞机为激活状态
+            if me.active:
+                # 在屏幕上绘制玩家飞机,switch_image为是否切换图片
+                if switch_image:
+                    screen.blit(me.image1, me.rect)
+                # 代表切换一下飞行图片
+                else:
+                    screen.blit(me.image2, me.rect)
+            # 代表飞机遭到碰撞，激活爆炸事件
+            else:
+                print("飞机损毁")
 
+            delay -= 1
+            if delay == 0:
+                delay = 100
+            # 每5帧切换一下飞行图片样式
+            if delay % 5 == 0:
+                switch_image = not switch_image
+
+        # 在屏幕上面绘制背景图像，并指定位置
+        screen.blit(me.image1, me.rect)
+
+        # 更新整个待显示的  Surface 对象到屏幕上，将内存中的内容显示到屏幕上
+        pygame.display.flip()
+        # 通过时钟对象指定循环频率，每秒循环60次
+        # 帧速率是指程序每秒在屏幕山绘制图
+        clock.tick(60)
+
+if __name__ == "__main__":
+    try:
+        main()
+    # 服务正常退出
+    except SystemExit:
+        print("游戏正常退出！")
+        # pass忽略错误并继续往下运行，其实这里以及退出了
+        pass
+    # 服务出现其他的异常
+    except:
+        # 直接将错误打印出来
+        traceback.print_exc()
+        pygame.quit()
 
 
