72f364ad04d267497c883f4facb06f7e5c6053f5
huyunlong007 <huyunlongxi@icloud.com>
2022-05-12 15:49:39 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/512.py
+++ Users/huyunlong/miniforge3/lib/hyl/512.py
@@ -1,4 +1,8 @@
 #操作列表
 cars=['dongfeng','yiqi','beiqi','hongqi']
-for car in cars:
+for car in cars: #for循环列表中的元素
     print(car)
+    print(car.title() + " is very good!") #for循环下缩进显示
+print("all cars is very good!") #结束循环最后输出
+
+
