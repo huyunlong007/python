c88b07cd53e1b5ef8e40bb8ecb647f8a7366c5b6
huyunlong007 <huyunlongxi@icloud.com>
2022-05-13 17:05:10 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/513.py
+++ Users/huyunlong/miniforge3/lib/hyl/513.py
@@ -13,3 +13,21 @@ for fruit in fruits[3:]: #遍历切片元素
 socres=[89,23,46,78,98,100,35,38]
 for score in socres[-3:]:
     print(score)
+
+#
+my_food=['pizza','beef','cake']
+my_friend_food=my_food[-2:] #把切片赋给；不是把全部赋给
+my_food.append('connoli')
+my_friend_food.append('ice cream')
+print("my favorite food is ")
+print(my_food)
+print("my friend favorite food is ")
+print(my_friend_food)
+
+#元组
+orders=('noodle','soop','fruit')
+for order in orders:
+    print(order)
+change_orders=('beef','banana') #不能修改元组里的元素，可以重新定义个元组
+for change_order in change_orders:
+    print(change_order)
