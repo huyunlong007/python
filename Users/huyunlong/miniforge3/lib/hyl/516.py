3db9504f55be0c6a873a24997b7da6a28e7bc36d
huyunlong007 <huyunlongxi@icloud.com>
2022-05-16 16:43:21 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/516.py
+++ Users/huyunlong/miniforge3/lib/hyl/516.py
@@ -7,22 +7,39 @@ for car in cars:
         print(car.title())
 
 #if-elif-else
-age=123
-if age<4:
+age = 123
+if age < 4:
     print("your admission is $0.")
-elif age<18:
+elif age < 18:
     print("your admission is $8.")
 else:
     print("your admission is $10.")
 
 #小练习
-alien_colors=['green','yellow','red']
+alien_colors = ['green','yellow','red']
 for alien_color in alien_colors:
-      if alien_color=='green':
+      if alien_color == 'green':
           print("players get 5 points.")
-      elif alien_color !="red":
+      elif alien_color != "red":
           print("players get 10 points.")
       else:
           print("thank you,please try again!")
 
-#
+#使用if语句处理列表
+requestd_toppings = []
+requestd_toppings.append("apple")
+if requestd_toppings:     #判断是否需要元素
+    for requestd_topping in requestd_toppings:
+        print("adding "+requestd_topping+".")
+    print("finished making your mizza!")
+else:
+    print("are you sure you want a plain pizza!")
+
+#多个列表
+cars = ['audi','bmw','benz']
+super_cars = ['ferrari','lamborghini','bugatti','bmw','benz']
+for car in cars:
+    if car in super_cars:
+        print("this super car is "+car)
+    else:
+        print("my favourite car is "+car)
