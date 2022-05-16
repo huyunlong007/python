b31a1314a7bd080b77872261b0585391dbf958f7
huyunlong007 <huyunlongxi@icloud.com>
2022-05-16 10:48:29 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/516.py
+++ Users/huyunlong/miniforge3/lib/hyl/516.py
@@ -6,5 +6,23 @@ for car in cars:
     else:
         print(car.title())
 
-#布尔表达式
+#if-elif-else
+age=123
+if age<4:
+    print("your admission is $0.")
+elif age<18:
+    print("your admission is $8.")
+else:
+    print("your admission is $10.")
 
+#小练习
+alien_colors=['green','yellow','red']
+for alien_color in alien_colors:
+      if alien_color=='green':
+          print("players get 5 points.")
+      elif alien_color !="red":
+          print("players get 10 points.")
+      else:
+          print("thank you,please try again!")
+
+#
