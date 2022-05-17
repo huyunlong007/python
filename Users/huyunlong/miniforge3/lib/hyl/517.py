2505c4b5d23f9b48d94d88f2e5c572b2ae4f414b
huyunlong007 <huyunlongxi@icloud.com>
2022-05-17 17:29:41 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/517.py
+++ Users/huyunlong/miniforge3/lib/hyl/517.py
@@ -43,5 +43,48 @@ for name in user_0:
     print(name)
     print(user_0[name].upper()) #输出对应key的值使用字典
 
+##按照顺序遍历字典sorted()方法
+favorite_language = {
+    'jay': 'python',
+    'kris': 'C#',
+    'jeray': 'java',
+}
+for name in sorted(favorite_language.keys()):  #sorted()函数按
+    print(name.title()+", thank you for taking the poll")
+
+favorite_language['tony'] = 'python' #字典添加键值
+print(favorite_language)
+#使用value()方法
+for language in favorite_language.values():
+    print(language.upper())
+for language in set(favorite_language.values()):  #使用set()方法去重复值
+    print(language.upper())
+
+####嵌套
+aliens = []
+for new_alien in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'fast'}
+    aliens.append(new_alien)
+    print(aliens)
+for alien in aliens[:-1]:
+    print(alien)
+print("totol number of aliens: "+str(len(aliens)))
+##
+aliens = []
+for new_alien in range(0,30):
+    new_alien = {'color': 'green', 'points': 7, 'speed': 'slow'}
+    aliens.append(new_alien)
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 27
+    elif alien['color'] == 'yellow':
+        alien['speed'] = 'fast'
+        alien['points'] = 28
+for alien in aliens[0:5]:
+    print(alien)
+
+
+
 
 
