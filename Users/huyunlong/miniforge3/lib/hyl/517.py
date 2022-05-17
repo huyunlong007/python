256c1a5bc745189214900bd8f486790054335b49
huyunlong007 <huyunlongxi@icloud.com>
2022-05-17 11:23:15 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/517.py
+++ Users/huyunlong/miniforge3/lib/hyl/517.py
@@ -1,5 +1,5 @@
 alien = {'x_position' :2,'y_position' :25,'speed': 'medium'}
-print("original x_position: "+str(alien['x_position']))
+print("original x_position: "+str(alien['x_position'])) #输出之前坐标
 if alien['speed'] == 'slow':
     x_increment = 1
 elif alien['speed'] == 'fast':
@@ -7,4 +7,13 @@ elif alien['speed'] == 'fast':
 else:
     x_increment = 2
 alien['x_position'] = alien['x_position'] + x_increment
-print("new x_position: "+str(alien['x_position']))
+print("new x_position: "+str(alien['x_position'])) #输出变化后坐标
+alien['points'] = 7  #添加一个键值
+print(alien)
+del alien['y_position']  #删除一个键值
+print(alien)
+
+##类似对象组成的字典
+
+
+
