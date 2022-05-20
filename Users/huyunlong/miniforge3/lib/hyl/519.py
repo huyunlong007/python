01707162787e2ce7c4932b310e5acfaa940ff268
huyunlong007 <huyunlongxi@icloud.com>
2022-05-20 09:12:03 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/519.py
+++ Users/huyunlong/miniforge3/lib/hyl/519.py
@@ -11,8 +11,12 @@ age = int(age) #把字符串转换成数值
 if age >= 18:
    print(age)
 #求模运算符
-
-
+number = input("enter a number: ")
+number = int(number)
+if number % 3 == 0:
+   print("this number "+str(number)+" is a even!")
+else:
+   print("this number "+str(number)+" is a odd!")
 
 
 
