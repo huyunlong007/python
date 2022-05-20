676bb28b9101f3b4fcfe97923f43c1dfa2d03aa8
huyunlong007 <huyunlongxi@icloud.com>
2022-05-20 09:04:18 +0800

    hyl

---
+++ Users/huyunlong/miniforge3/lib/hyl/519.py
@@ -0,0 +1,23 @@
+#函数input()
+name = input("please enter your name: ")
+print("hello, "+name)
+#
+birthday_wish = "enter your name: "
+name = input(birthday_wish)
+print("happy birthday!"+name)
+
+age = input("enter your age")
+age = int(age) #把字符串转换成数值
+if age >= 18:
+   print(age)
+#求模运算符
+
+
+
+
+
+
+
+
+
+
