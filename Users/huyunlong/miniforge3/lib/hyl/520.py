627c5c4bc5519153de83c420f71ec63b88564d8f
huyunlong007 <huyunlongxi@icloud.com>
2022-05-20 15:50:49 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/520.py
+++ Users/huyunlong/miniforge3/lib/hyl/520.py
@@ -1,1 +1,34 @@
 #whlie循环
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+#1到100相加
+sum = 0
+i = 0
+while i <= 100:
+    sum = sum + i
+    i += 1
+print(sum)
+
+#
+'''
+prompt = "tell me something,and i will repeat it back to you: "
+prompt += "\nenter the 'quit' to end the program " #+=是上面输入下面提示
+message = ''   #创建变量，定义一个初始值
+while message != 'quit':   #判断变量并循环
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+'''
+#使用标志
+
+
+
+
+
+
+
+
+
