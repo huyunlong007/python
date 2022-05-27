a572fa09766e7d3db9fd114d2869d3fdbef709a9
huyunlong007 <huyunlongxi@icloud.com>
2022-05-27 09:41:48 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/527.py
+++ Users/huyunlong/miniforge3/lib/hyl/527.py
@@ -1,12 +1,4 @@
-#传递列表
-def greet_users(names):
-    for name in names:
-        msg = "hello, "+name.title()+"!"
-        print(msg)
-usernames = ['tony', 'jerry']
-greet_users(usernames)
-
-#在函数中修改列表
+#禁止修改函数列表
 def print_models(unprinted_designs, completed_models):  #定义一个print_model函数
     while unprinted_designs:
         current_design = unprinted_designs.pop()
@@ -18,23 +10,5 @@ def show_completed_models(completed_models):
         print(completed_model)
 unprinted_designs = ['iphone case', 'robot']
 completed_models = []
-print_models(unprinted_designs,completed_models)
+print_models(unprinted_designs[:],completed_models)
 show_completed_models(completed_models)
-
-#禁止修改函数列表
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
