0b78d35c72525a6222d9d1259d0daeddc80a6ed2
huyunlong007 <huyunlongxi@icloud.com>
2022-05-30 13:59:37 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/529_01.py
+++ Users/huyunlong/miniforge3/lib/hyl/529_01.py
@@ -9,6 +9,8 @@ class Car():
         print(self.make+" "+self.model+" "+str(self.year))
     def read_odometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
 
 my_new_car = Car('BMW', 'M5 comptation', 2020)
 print(my_new_car.get_descirptive_name())
@@ -19,6 +21,13 @@ my_new_car = Car('Audi', 'A7', 2021)
 print(my_new_car.get_descirptive_name())
 my_new_car.odometer_reading = 520
 my_new_car.read_odometer()
+#
+my_new_car = Car('Benz', 'E300', 2020)
+print(my_new_car.get_descirptive_name())
+my_new_car.update_odometer(27)
+my_new_car.read_odometer()
+
+
 
 
 
