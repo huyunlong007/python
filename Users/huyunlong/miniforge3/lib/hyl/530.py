3606af8ddefebef011852a0a03ef183fc86388b4
huyunlong007 <huyunlongxi@icloud.com>
2022-05-31 09:30:20 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/530.py
+++ Users/huyunlong/miniforge3/lib/hyl/530.py
@@ -25,6 +25,39 @@ class ElectricCar(Car):   #在定义子类必须在括号指定父类的名称
         self.battery_size = 70  #添加一个新的属性并设置默认值
     def describe_battery(self):
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    '''重写父类的方法'''
+    def fill_gas_tank(self):
+        print("this car doesn't need a gas tank!")
+
+class Battery():
+    def __init__(self,battery_siza = 90):
+        self.battery_size = battery_siza
+    def descibe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    def get_range(self):
+        if self.battery_size == 90:
+            range = 240
+        elif self.battery_size == 100:
+            range = 270
+        message = "this car can go "+str(range)
+        message += " miles on a full charge"
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
+'''
 my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
+my_tesla.update_odometer(235)
+my_tesla.read_odometer()
+my_tesla.increment_odometer(100)
+my_tesla.read_odometer()
+'''
+my_tesla.battery.descibe_battery()
+my_tesla.battery.battery_size = 100
+my_tesla.battery.get_range()
