49773f9d9066918d9c142b08900cef2bc8b3300b
huyunlong007 <huyunlongxi@icloud.com>
2022-06-01 17:09:48 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/601.py
+++ Users/huyunlong/miniforge3/lib/hyl/601.py
@@ -4,4 +4,9 @@ my_new_car = Car('Audi', 'a7', 2022)
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
-
+#导入多个类
+from car import Car,ElectricCar
+my_beetle = Car('BMW', 'M5', 2022)
+print(my_beetle.get_descriptive_name())
+my_tesla = ElectricCar('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
