cb203b8f2978683c138f4f828e4bc0330b29be48
huyunlong007 <huyunlongxi@icloud.com>
2022-06-04 19:26:33 +0800

    hyl

--- Users/huyunlong/miniforge3/lib/hyl/604.py
+++ Users/huyunlong/miniforge3/lib/hyl/604.py
@@ -1,5 +1,10 @@
 ##写入文件
 filename = 'programing.txt'
-with open(filename, 'w') as file_object:  #第一个实参为'w'写入模式打开文件
+with open(filename, 'a') as file_object:  #第一个实参为'w'写入模式打开文件
     file_object.write("i love you.\n")
     file_object.write("i will be work out.\n")
+
+##小练习
+name = 'guest.txt'
+with open(name, 'w') as file_name:
+    file_name.write("what's your name")
